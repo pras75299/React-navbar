f8bec0cbef18afc6c8618823eb042e935a192345
pras75299 <singh.prashantking@gmail.com>
2021-05-02 23:08:51 +0530

    add navbar basic structure

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,8 +10,33 @@ const Navbar = () => {
       <img src={logo} alt="Logo" />
       <button className="nav-toggle"><FaBars/></button>
     </div>
-    <div className="links-container show-container"></div>
-    <ul className="social-icons"></ul>
+    <div className="links-container show-container">
+      <ul className="links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+    <ul className="social-icons">
+      <li>
+        <a href="https://twitter.com/home"><FaTwitter/></a>
+      </li>
+      <li>
+        <a href="https://twitter.com/home">Contact</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/home">Contact</a>
+      </li>
+    </ul>
   </div>
   </nav>
 }
