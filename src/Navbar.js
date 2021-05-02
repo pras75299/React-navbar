849d7bded0ceb6ca8e0d8d6008a1a0fb48e66165
pras75299 <singh.prashantking@gmail.com>
2021-05-02 23:02:05 +0530

    add navbar component to app.js and add logo and toggle button to navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,16 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return <nav>
+  <div className="nav-center">
+    <div className="nav-header">
+      <img src={logo} alt="Logo" />
+      <button className="nav-toggle"><FaBars/></button>
+    </div>
+    <div className="links-container show-container"></div>
+    <ul className="social-icons"></ul>
+  </div>
+  </nav>
 }
 
 export default Navbar
