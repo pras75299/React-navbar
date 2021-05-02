c4e6d386f1fe4428d649ba02d72bd901b74c6d25
pras75299 <singh.prashantking@gmail.com>
2021-05-02 23:47:39 +0530

    dynamically add classes to show and hide link-container with animation

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,34 +1,43 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
-  return <nav>
-  <div className="nav-center">
-    <div className="nav-header">
-      <img src={logo} alt="Logo" />
-      <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}><FaBars/></button>
-    </div>
-    {showLinks && <div className="links-container show-container">
-      <ul className="links">
-        {links.map((link) => {
-          const {id, url, text} = link;
-          return <li key={id}>
-            <a href={url}>{text}</a></li>
+  return (
+  <nav>
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="Logo" />
+        <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
+          <FaBars />
+        </button>
+      </div>
+      <div className={`${showLinks ? 'links-container show-container' : 'links-container'}`}>
+        <ul className="links">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {social.map((socialLinks) => {
+          const { id, url, icon } = socialLinks;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
         })}
       </ul>
-    </div>}
-    <ul className="social-icons">
-        {social.map((socialLinks) => {
-          const {id, url, icon} = socialLinks;
-          return <li key={id}>
-            <a href={url}>{icon}</a></li>
-        })}
-    </ul>
-  </div>
-  </nav>
-}
+    </div>
+  </nav>)
+};
 
-export default Navbar
+export default Navbar;
