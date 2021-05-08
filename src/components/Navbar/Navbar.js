18abb2d6d0cd4b64e6b05827939b125f51241461
pras75299 <singh.prashantking@gmail.com>
2021-05-08 21:05:17 +0530

    add components folder for all ui components at one place and edit the application structure

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
-import { links, social } from "./data";
-import logo from "./logo.svg";
+import { links, social } from "./data.js";
+import logo from "../../logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
